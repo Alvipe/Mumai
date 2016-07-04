cd8daf2fa83e59d54336fbc57ad2b4861049a754
Alvipe <alvipe5@protonmail.com>
2016-07-04 11:23:40 +0200

    Update emg_clf_test.py and EMG dataset

--- Gesture classification/emg_clf_test.py
+++ Gesture classification/emg_clf_test.py
@@ -6,16 +6,16 @@ Created on Tue Nov 10 09:03:53 2015
 """
 
 from emgesture import fextraction as fex
-from emgesture import classifier as clf
-import scipy.io as sio
 import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import SVC
 
-### Data loading ###
-emg_data = sio.loadmat('emg_data_5_class_2ch')
-#emg_data = sio.loadmat('emg_25-11-15')
+# Data loading
+emg_data = np.load('emg_data_5class_2channel.npy').item()
+
 nGestures = 5
 nChannels = 2
-nIterations = 4
+nIterations = 5
 emg = []
 segmented_emg = []
 
@@ -24,77 +24,51 @@ for m in range(1,nGestures+1):
         for c in range(1,nChannels+1):
             emg.append(emg_data['motion'+str(m)+'_ch'+str(c)][:,i]) #motion1_ch1_i1, motion1_ch2_i1, motion1_ch1_i2, motion1_ch2_i2
 
-### Segmentation ###
-for n in range(len(emg)):
+nSignals = len(emg)
+
+# Segmentation
+for n in range(nSignals):
     segmented_emg.append(fex.segmentation(emg[n]))
 
-### Feature calculation ###
+# Feature calculation
 feature_list = [fex.mav, fex.rms, fex.var, fex.ssi, fex.zc, fex.wl, fex.ssc, fex.wamp]
 
 nSegments = len(segmented_emg[0][0])
 nFeatures = len(feature_list)
-
-feature_matrix = np.zeros((nSegments*nIterations*nGestures,nFeatures*nChannels))
+feature_matrix = np.zeros((nSegments*nGestures*nIterations,nFeatures*nChannels))
 n = 0
 
-for i in range(0,len(segmented_emg),nChannels):
+for i in range(0,nSignals,nChannels):
     for j in range(nSegments):
         feature_matrix[n] = fex.features((segmented_emg[i][:,j],segmented_emg[i+1][:,j]),feature_list)
-        n += 1
-
-import timeit
-
-### Target matrix generation ###
-y = fex.gestures(nIterations*nSegments,nGestures)
-
-tic = timeit.default_timer()
-
-### Feature scaling ###
-[X,reductor,scaler] = fex.feature_scaling(feature_matrix, y)
-
-### Classifier training ###
-classifier = clf.train(X,y)
-
-toc = timeit.default_timer()
-
-print("Feature transformation + training time = %0.5f s." %(toc - tic))
-
-emg_test = []
-segmented_emg_test = []
-
-for m in range(1,nGestures+1):
-    for c in range(1,nChannels+1):
-        emg_test.append(emg_data['motion'+str(m)+'_ch'+str(c)][:,4]) #motion1_ch1_i1, motion1_ch2_i1, motion1_ch1_i2, motion1_ch2_i2
-
-### Segmentation ###
-for n in range(len(emg_test)):
-    segmented_emg_test.append(fex.segmentation(emg_test[n]))
-
-### Feature calculation ###
-nIterations = 1 #To test the classifier, the last iteration of each gesture is used
-nSegments = len(segmented_emg_test[0][0])
-
-feature_matrix_test = np.zeros((nSegments*nIterations*nGestures,nFeatures*nChannels))
-n = 0
-
-for i in range(0,len(segmented_emg_test),nChannels):
-    for j in range(len(segmented_emg_test[0][0])):
-        feature_matrix_test[n] = fex.features((segmented_emg_test[i][:,j],segmented_emg_test[i+1][:,j]),feature_list)
         n = n + 1
 
-### Target matrix generation ###
-y_test = fex.gestures(nSegments,nGestures)
+# Target matrix generation
+y = fex.gestures(nSegments*nIterations,nGestures)
 
-tic = timeit.default_timer()
+# Dimensionality reduction and feature scaling
+[X,reductor,scaler] = fex.feature_scaling(feature_matrix, y)
 
-### Feature scaling ###
-[X_test,reductor,scaler] = fex.feature_scaling(feature_matrix_test,y_test,reductor,scaler)
+# Split dataset into training and testing datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
-### Classification ###
-predict = clf.classify(X_test,classifier)
+# Classifier training
+classifier = SVC(kernel='rbf',C=10,gamma=10)
+classifier.fit(X_train,y_train)
 
-toc = timeit.default_timer()
-
-print("Feature transformation + classification time = %0.5f s." %(toc - tic))
-
+# Classification
+predict = classifier.predict(X_test)
 print("Classification accuracy = %0.5f." %(classifier.score(X_test,y_test)))
+
+## Cross validation (optional; takes a lot of time)
+#from sklearn.cross_validation import StratifiedShuffleSplit
+#from sklearn.grid_search import GridSearchCV
+#from sklearn.svm import SVC
+#
+#C_range = np.logspace(-5,5,11)
+#gamma_range = np.logspace(-30,1,32)
+#param_grid = dict(gamma=gamma_range,C=C_range)
+#cv = StratifiedShuffleSplit(y, n_iter=20,test_size=0.2,random_state=42)
+#grid = GridSearchCV(SVC(),param_grid=param_grid,cv=cv)
+#grid.fit(X,y)
+#print("The best parameters are %s with a score of %0.2f" % (grid.best_params_,grid.best_score_))
